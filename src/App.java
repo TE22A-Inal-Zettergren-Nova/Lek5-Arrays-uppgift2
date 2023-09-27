d88e8f8f9296c033f71536641c83eb9b62be5051
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-09-27 10:02:39 +0200

    skapat scanner som ta rin heltal  från tb

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        int[] tb_fält = new int[4];
+
+        Scanner tb = new Scanner(System.in);
+        System.out.print("Skriv in Element 0: ");
+        int tal0 = tb.nextInt();
+        System.out.print("Skriv in Element 1: ");
+        int tal1 = tb.nextInt();
+        System.out.print("Skriv in Element 2: ");
+        int tal2 = tb.nextInt();
+        System.out.print("Skriv in Element 3: ");
+        int tal3 = tb.nextInt();
+
         
     }
 }
