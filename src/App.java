f42b8c9c51ffdaec14300a9731a03d01054b53d8
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-09-27 10:09:55 +0200

    elementen i fältet har bytt platser

--- src/App.java
+++ src/App.java
@@ -3,17 +3,27 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         int[] tb_fält = new int[4];
-
+        /*Uppgift a*/
         Scanner tb = new Scanner(System.in);
-        System.out.print("Skriv in Element 0: ");
-        int tal0 = tb.nextInt();
-        System.out.print("Skriv in Element 1: ");
+        System.out.print("Skriv in heltal 1: ");
         int tal1 = tb.nextInt();
-        System.out.print("Skriv in Element 2: ");
+        System.out.print("Skriv in heltal 2: ");
         int tal2 = tb.nextInt();
-        System.out.print("Skriv in Element 3: ");
+        System.out.print("Skriv in heltal 3: ");
         int tal3 = tb.nextInt();
+        System.out.print("Skriv in heltal 4: ");
+        int tal4 = tb.nextInt();
 
-        
+        tb_fält[0]= tal1;
+        tb_fält[1]= tal2;
+        tb_fält[2]= tal3;
+        tb_fält[3]= tal4;
+        System.out.println( tb_fält[0]+" "+ tb_fält[1]+" "+ tb_fält[2]+" "+ tb_fält[3]);
+
+        tb_fält[0]= tal4;
+        tb_fält[1]= tal3;
+        tb_fält[2]= tal2;
+        tb_fält[3]= tal1;
+        System.out.println( tb_fält[0]+" "+ tb_fält[1]+" "+ tb_fält[2]+" "+ tb_fält[3]);
     }
 }
